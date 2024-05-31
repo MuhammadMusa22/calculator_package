0f9606ec632506d3f5e9d9fee5ed4c2214ae3265
Karahan Osman <karahan.osman@epppuls.com>
2024-05-31 09:37:18 +0200

    Added controller options to the calculator to execute function programmatically

---
+++ lib/controller/CalculatorController.dart
@@ -0,0 +1,7 @@
+/// A class where the available controller function got declared
+class CalculatorController {
+
+  /// Clears the current answer, expression and the user input
+  Function()? clear;
+
+}
